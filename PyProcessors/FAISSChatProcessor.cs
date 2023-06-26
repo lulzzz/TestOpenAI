353a6c0e04a3b3a478f7aa0530edbdee402e6699
Pallab Gupta <88669723+pallabgupta@users.noreply.github.com>
2023-06-26 19:06:11 -0400

    Added buffermemory

--- PyProcessors/FAISSChatProcessor.cs
+++ PyProcessors/FAISSChatProcessor.cs
@@ -112,6 +112,7 @@ namespace PyProcessors
             //OnProcessCompleted?.Invoke(this, success);
             return qa;
         }
+        
         public string? AskQuestion(dynamic qaChat, string question)
         {
             using (Py.GIL())
@@ -145,19 +146,128 @@ namespace PyProcessors
             }
                 
         }
-        //public string? AskAQuestion(string faissIndexModelFilePath, string question)
-        //{
-        //    using (Py.GIL())
-        //    {
-        //        using var scope = Py.CreateScope();
+        public dynamic GetConversationModule(string faissIndexModelPath)
+        {
+            dynamic qa = null;
+            using (Py.GIL())
+            {
+                using var scope = Py.CreateScope();
+                try
+                {
+                    dynamic vectorstores = scope.Import("langchain.vectorstores");
+                    dynamic chains = scope.Import("langchain.chains");
+                    dynamic question_answering = scope.Import("langchain.chains.question_answering");
+                    dynamic chat_models = scope.Import("langchain.chat_models");
+                    dynamic openApiLangChain = scope.Import("langchain.embeddings.openai");
+                    dynamic openai = scope.Import("openai");
+                    dynamic prompts = scope.Import("langchain.prompts");
+                    dynamic memory = scope.Import("langchain.memory");
 
-        //        var qa = GetChatModule(scope, faissIndexModelFilePath);
-        //        var result = AskQuestion(qa, question);
-        //        var answer = result["result"];
-        //        var pyString = new PyString(answer);
-        //        return pyString.ToString();
-        //    }
-        //}
+
+                    openai.api_type = "azure";
+                    openai.api_version = OPENAI_DEPLOYMENT_VERSION;
+                    openai.api_base = OPENAI_DEPLOYMENT_ENDPOINT;
+                    openai.api_key = OPENAI_API_KEY;
+
+                    dynamic llm = chat_models.AzureChatOpenAI(
+                        Py.kw("deployment_name", OPENAI_DEPLOYMENT_NAME),
+                        Py.kw("model_name", OPENAI_MODEL_NAME),
+                        Py.kw("openai_api_base", OPENAI_DEPLOYMENT_ENDPOINT),
+                        Py.kw("openai_api_version", OPENAI_DEPLOYMENT_VERSION),
+                        Py.kw("openai_api_key", OPENAI_API_KEY)
+                        );
+                    dynamic embeddings = openApiLangChain.OpenAIEmbeddings(
+                        Py.kw("model", OPENAI_EMBEDDING_MODEL_NAME),
+                        Py.kw("deployment", OPENAI_EMBEDDING_MODEL_NAME),
+                        Py.kw("chunk_size", 1),
+                        Py.kw("openai_api_key", OPENAI_API_KEY)
+                    );
+
+                    //load the faiss vector store we saved into memory
+                    dynamic vectorStore = vectorstores.FAISS.load_local(faissIndexModelPath, embeddings);
+
+                    //use the faiss vector store we saved to search the local document
+                    dynamic retriever = vectorStore.as_retriever();//
+                        //Py.kw("search_type", "similarity"),
+                        ////Py.kw("search_kwargs", "{ \"k\":2}")
+                        //Py.kw("search_kwargs", Py.kw("k", 2))
+                        //);
+
+                    var promptTemplate = @"""Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+                            Chat History:
+                            {chat_history}
+                            Follow Up Input: {question}
+                            Standalone question:""";
+                    dynamic CONDENSE_QUESTION_PROMPT = prompts.PromptTemplate.from_template(promptTemplate);
+                    var bufferMemory = memory.ConversationBufferMemory(Py.kw("memory_key", "chat_history"), Py.kw("return_messages", true.ToPython()));
+                    qa = chains.ConversationalRetrievalChain.from_llm(
+                        Py.kw("llm", llm),
+                        Py.kw("retriever", retriever),
+                        Py.kw("condense_question_prompt", CONDENSE_QUESTION_PROMPT),
+                        Py.kw("return_source_documents", false.ToPython()),
+                        Py.kw("verbose", false.ToPython()),
+                        Py.kw("memory", bufferMemory)
+                        ); ;
+
+                }
+                catch (Exception ex)
+                {
+
+                    //throw;
+                }
+                return qa;
+            }
+        }
+        public dynamic AskConversationQuestion(dynamic qaConversation, string question, string? lastQuestion = null, string? lastAnswer = null)
+        {
+            using (Py.GIL())
+            {
+                using var scope = Py.CreateScope();
+                dynamic openai = scope.Import("openai");
+                openai.api_type = "azure";
+                openai.api_version = OPENAI_DEPLOYMENT_VERSION;
+                openai.api_base = OPENAI_DEPLOYMENT_ENDPOINT;
+                openai.api_key = OPENAI_API_KEY;
+                //var result = qaChat({ "query": question});
+                try
+                {
+                    OnProcessStarted?.Invoke(this, "Thinking...");
+
+                    //var chatHistory = Array.Empty<(string, string)>();
+                    //if (!string.IsNullOrWhiteSpace(lastQuestion) && !string.IsNullOrWhiteSpace(lastAnswer))
+                    //{
+                    //    Array.Fill<(string, string)>(chatHistory, (lastQuestion, lastAnswer));
+                    //}
+                    //var chat_history = chatHistory.ToPython();
+                    //var qry = $"{{'question': '{question}', 'chat_history': '{lastAnswer}' }}";
+                    dynamic result = qaConversation(Py.kw("inputs", Py.kw("question", question)));
+
+                    
+                    
+                    //var parameters = new[]
+                    //{
+                    //    Py.kw("question", question),
+                    //    Py.kw("chat_history", chat_history)
+                    //};
+                    //dynamic result = qaConversation(Py.kw("inputs", args));
+                    
+                    //return result;
+                    var answer = result["answer"];
+                    var pyString = new PyString(answer);
+                   
+                    //return result;
+                    OnProcessCompleted?.Invoke(this, true);
+                    return pyString.ToString();
+                }
+                catch (Exception ex)
+                {
+                    OnProcessCompleted?.Invoke(this, false);
+                    throw;
+                }
+            }
+                
+        }
         public void Dispose()
         {
             //scope?.Dispose();

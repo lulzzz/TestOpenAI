4afdafa7f2c1536b46662674adaa6322995c25b5
Pallab Gupta <88669723+pallabgupta@users.noreply.github.com>
2023-06-26 15:42:09 -0400

    Updated PdfProcessor uploadfiles

--- PyProcessors/PdfProcessor.cs
+++ PyProcessors/PdfProcessor.cs
@@ -73,11 +73,15 @@ namespace PyProcessors
                         Py.kw("chunk_size", 1),
                         Py.kw("openai_api_key", OPENAI_API_KEY)
                         );
+
+                    
+
                     dynamic loader = pyPdf.PyPDFLoader(fileName);
 
                     OnProcess?.Invoke(this, "Splitting document to pages");
-                    var pages = loader.load_and_split();//
 
+                    var pages = loader.load_and_split();//
+                    
                     OnProcess?.Invoke(this, "Indexing document");
                     dynamic db = vectorchains.FAISS.from_documents(Py.kw("documents", pages), Py.kw("embedding", embeddings));
 
@@ -95,6 +99,80 @@ namespace PyProcessors
                 throw;
             }
             
+        }
+        public bool IndexExists(string indexName, string dirIndex)
+        {
+            var fileIndex = indexName.CleanString();
+            var indexPath = Path.Combine(dirIndex, fileIndex);
+
+            return Directory.Exists(indexPath) && Directory.GetFiles(indexPath).Any();
+        }
+        public string ProcessPdfs(string indexName,string dirIndex, IEnumerable<UploadFile> uploadFiles)
+        {
+            try
+            {
+                var fileIndex = indexName.CleanString();
+                var indexPath = Path.Combine(dirIndex, fileIndex);
+                if (!Directory.Exists(indexPath))
+                {
+                    Directory.CreateDirectory(indexPath);
+                }
+                using var _ = Py.GIL();
+                using var scope = Py.CreateScope();
+
+                OnProcess?.Invoke(this, "Importing Python libraries");
+
+                dynamic pyPdf = scope.Import("langchain.document_loaders.pdf");
+                dynamic openApiLangChain = scope.Import("langchain.embeddings.openai");
+                dynamic openai = scope.Import("openai");
+                dynamic vectorchains = scope.Import("langchain.vectorstores");
+
+                OnProcess?.Invoke(this, "Initializing AI");
+                //initialize azure openai
+                openai.api_type = "azure";
+                openai.api_version = OPENAI_DEPLOYMENT_VERSION;
+                openai.api_base = OPENAI_DEPLOYMENT_ENDPOINT;
+                openai.api_key = OPENAI_API_KEY;
+
+                OnProcess?.Invoke(this, "Creating embeddings");
+                dynamic embeddings = openApiLangChain.OpenAIEmbeddings(
+                    Py.kw("model", OPENAI_EMBEDDING_MODEL_NAME),
+                    Py.kw("chunk_size", 1),
+                    Py.kw("openai_api_key", OPENAI_API_KEY)
+                    );
+                dynamic allPages = null;
+                foreach (var uploadFile in uploadFiles)
+                {
+                    //check for pdf and then add appropriate loader
+                    dynamic loader = pyPdf.PyPDFLoader(uploadFile.FilePath);
+                    OnProcess?.Invoke(this, $"Splitting {Path.GetFileName(uploadFile.FilePath)}");
+                    var pages = loader.load_and_split();//
+                    if (allPages == null)
+                        allPages = pages;
+                    else
+                        allPages.extend(pages);
+                }
+
+                OnProcess?.Invoke(this, "Indexing...");
+                dynamic db = vectorchains.FAISS.from_documents(Py.kw("documents", allPages), Py.kw("embedding", embeddings));
+
+                OnProcess?.Invoke(this, "Saving index");
+                db.save_local(indexPath);
+
+                OnCompleted?.Invoke(this, true);
+                return Path.GetFileName(indexPath);
+                //clean former indexes
+                //var existingFiles = new DirectoryInfo(indexPath).GetFiles();
+                //foreach ( var existingFile in existingFiles )
+                //{
+                //    File.Delete(existingFile.FullName);
+                //}
+            }
+            catch (Exception ex)
+            {
+                OnCompleted?.Invoke(this, false);
+                throw;
+            }
         }
     }
 }
---
+++ PyProcessors/UploadFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyProcessors
+{
+    public class UploadFile
+    {
+        public string FilePath { get; set; }
+        public string ContentType { get; set; }
+    }
+}
